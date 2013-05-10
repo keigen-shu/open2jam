bb592ef81f180efb1e8c0de1a890167ed7a2c478
Keigen Shu <keigen.shu@gmail.com>
2013-05-10 12:07:34 +0800

    Fix ordering on Common spritesheets.

--- src/resources/Common/compile.sh
+++ src/resources/Common/compile.sh
@@ -1,17 +1,17 @@
 #/bin/sh
 
 echo "Compiling Long Note Effect..."
-j=$(find LongNote_Effect__*.bmp -type f | sort -t_ -n -k5,5 | xargs)
+j=$(find LongNote_Effect__*.bmp -type f | sort -t_ -n -k4,4 | xargs)
 montage $j -geometry +0+0 LongNote_Effect.png
 
 echo "Compiling LX#.# sprites..."
-j=$(find main_level__*.bmp -type f | sort -t_ -n -k5,5 | xargs)
+j=$(find main_level__*.bmp -type f | sort -t_ -n -k4,4 | xargs)
 montage $j -geometry +0+0 main_level.png
 
 echo "Compiling Jam Level counter numbers and text..."
-j=$(find Note_Jam__*.bmp -type f | sort -t_ -n -k5,5 | xargs)
+j=$(find Note_Jam__*.bmp -type f | sort -t_ -n -k4,4 | xargs)
 montage $j -geometry '1x1+0+0<' Note_Jam.png
-j=$(find Note_JamNum__*.bmp -type f | sort -t_ -n -k5,5 | xargs)
+j=$(find Note_JamNum__*.bmp -type f | sort -t_ -n -k4,4 | xargs)
 montage $j -geometry '1x1+0+0<' Note_JamNum.png
 
 echo "Compiling Exit button..."
